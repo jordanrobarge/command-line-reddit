a0e00d9940d0343f9a430b6d31fc95ba05731dc1
Logan Hasson <logan.hasson@gmail.com>
2013-10-02 23:35:07 -0400

    Added ability to go to a different subreddit.

--- command.line.reddit.rb
+++ command.line.reddit.rb
@@ -31,11 +31,12 @@ class Post
 
   def self.launch_story(path)
     post_to_open = POSTS.select { |post| post.id == path }[0]
-    if !post_to_open
-      puts "I don't recognize that story id. Please try again."
-      sleep(1.2)
-    else
-      `open #{post_to_open.permalink}`
+    `open #{post_to_open.permalink}`
+  end
+
+  def self.clear_posts
+    while POSTS.size > 0
+      POSTS.pop
     end
   end
 
@@ -54,6 +55,7 @@ end
 
 while true
   system("clear")
+  visible_posts = []
   longest_votes_diff = 0
   Post::POSTS.each do |post|
     if post.votes_diff.to_s.length > longest_votes_diff
@@ -62,21 +64,28 @@ while true
   end
 
   Post::POSTS.each do |post|
-    puts post.id.blue + '  ' + post.votes_diff.to_s.ljust(longest_votes_diff, " ").green + '  ' + post.title[0..60].black
+    puts post.id.blue + '  ^'.green + post.votes_diff.to_s.ljust(longest_votes_diff, " ").green + '  ' + post.title[0..60].black
+    visible_posts << post.id
   end
   puts ""
   print "Enter a story id, or type 'exit' to quit: "
   path = $stdin.gets.chomp
 
-  if path == 'exit'
+  if path == 'exit' || path == ''
     break
-  else
+  elsif visible_posts.include?(path)
     Post.launch_story(path)
+  elsif path[0].to_i.to_s == path[0]
+    puts "I don't recognize that story id. Please try again."
+    sleep(1.2)
+  else
+    Post.clear_posts
+    puts "Loading..."
+    reddit_hash = JSON.parse(RestClient.get('http://reddit.com/r/' + path + '.json'))
+    reddit_hash["data"]["children"].each do |post|
+      unless post["data"]["over_18"] == true
+        make_posts(post)
+      end
+    end
   end
-
 end
-
-
-# go through upvotes
-# figure out longest one
-# ljust(length)
